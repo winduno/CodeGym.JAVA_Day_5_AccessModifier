2bfcdb35185a29bf75d8158b086a35806848a238
winduno <windsolo294@gmail.com>
2020-09-16 08:12:48 +0700

    add

---
+++ src/Circle.java
@@ -0,0 +1,19 @@
+public class Circle {
+    private double  radius = 1.0;
+    private String color = "red";
+
+    public Circle(){
+
+    }
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
